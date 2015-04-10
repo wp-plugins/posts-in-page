f2226c4d6a6eb39fec8ac8da42161c7253abdb78
sewmyheadon <sewmyheadon@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-10 02:28:17 +0000

    Update readme and version numbers
    git-svn-id: https://plugins.svn.wordpress.org/posts-in-page/trunk@1131677 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- posts_in_page.php
+++ posts_in_page.php
@@ -9,24 +9,23 @@
  *  version: 1.2.4
  *  License: GNU General Public License v2.0
  *  License URI: http://www.gnu.org/licenses/gpl-2.0.html
-
- ------------------------------------------------------------------------
-	IvyCat Posts in Page, Copyright 2012 -2013 IvyCat, Inc. (admins@ivycat.com)
-
-	This program is free software; you can redistribute it and/or modify
-	it under the terms of the GNU General Public License as published by
-	the Free Software Foundation; either version 2 of the License, or
-	(at your option) any later version.
-
-	This program is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-	GNU General Public License for more details.
-
-	You should have received a copy of the GNU General Public License
-	along with this program; if not, write to the Free Software
-	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
-
+ *
+ * ------------------------------------------------------------------------
+ *	IvyCat Posts in Page, Copyright 2012 - 2015 IvyCat, Inc. (wp@ivycat.com)
+ *
+ *	This program is free software; you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation; either version 2 of the License, or
+ *	(at your option) any later version.
+ *
+ *	This program is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with this program; if not, write to the Free Software
+ *	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  */
 
 if ( !function_exists( 'add_action' ) )
